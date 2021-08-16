c24ca1638a57e28f492e7bad861756295f34e86c
Zapzor <imp.4xlbm@aleeas.com>
2021-08-16 12:14:19 +0100

    Made function that allows user to choose grid size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,37 @@
-const container = document.querySelector('#container')
-
-function makeDiv() {
-    for (i = 0; i < 256; i++) {
-        let div = document.createElement('div');
-        div.setAttribute('style', 'color: blue; width: 25px; height: 25px; background: orange');    
-        div.addEventListener('mouseenter', function(e) {
+//Variables
+const container = document.querySelector('#container');
+const refresh = document.querySelector('#btn');
+let div; 
+makeGridDiv(256)
+//Creates 256 individual divs for 16x16 etch sketch grid
+function makeGridDiv(rowCol) {
+    for (i = 0; i < rowCol; i++) {
+        div = document.createElement('div');
+        div.setAttribute('style','width: 30px; height: 30px; background: orange');    
+        div.addEventListener('mouseover', function(e) {
             e.target.style.backgroundColor = 'white';
         });
         container.appendChild(div)
 }}
-makeDiv()
+
+//Button that refreshes grid when clicked
+function refreshGrid(rowCol){
+
+   rowCol = prompt('How many squares would you like to use?')
+   document
+   .querySelectorAll('div')
+   .forEach((div) => div.parentNode.removeChild(div));
+   for (i = 0; i < rowCol * rowCol; i++) {
+       if(rowCol < 100) {
+        div = document.createElement('div');
+        div.setAttribute('style','width: 30px; height: 30px; background: orange');    
+        div.addEventListener('mouseover', function(e) {
+        e.target.style.backgroundColor = 'white';
+        container.appendChild(div)
+    })}else {
+        alert('Please choose a number less than 100');
+        break;
+    }
+}}
+refresh.addEventListener('click', (refreshGrid));
+

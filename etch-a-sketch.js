1f1db41f94582b795e9e44884e4a1dc7236cb3a5
Zapzor <imp.4xlbm@aleeas.com>
2021-08-16 12:59:38 +0100

    Added basic refresh button that allows user to reset grid and choose amount of squares to use

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,37 +1,36 @@
 //Variables
 const container = document.querySelector('#container');
 const refresh = document.querySelector('#btn');
+const choose = document.querySelector('#chooze');
+refresh.addEventListener('click', (refreshGrid));
 let div; 
-makeGridDiv(256)
+makeGridDiv(16)
 //Creates 256 individual divs for 16x16 etch sketch grid
 function makeGridDiv(rowCol) {
-    for (i = 0; i < rowCol; i++) {
-        div = document.createElement('div');
-        div.setAttribute('style','width: 30px; height: 30px; background: orange');    
-        div.addEventListener('mouseover', function(e) {
+    for (i = 0; i < rowCol * rowCol; i++) {
+        divs = document.createElement('divs');
+        divs.setAttribute('style','width: 30px; height: 30px; background: orange');    
+        divs.addEventListener('mouseover', function(e) {
             e.target.style.backgroundColor = 'white';
         });
-        container.appendChild(div)
+        container.appendChild(divs)
 }}
 
 //Button that refreshes grid when clicked
-function refreshGrid(rowCol){
-
-   rowCol = prompt('How many squares would you like to use?')
-   document
-   .querySelectorAll('div')
-   .forEach((div) => div.parentNode.removeChild(div));
-   for (i = 0; i < rowCol * rowCol; i++) {
-       if(rowCol < 100) {
-        div = document.createElement('div');
-        div.setAttribute('style','width: 30px; height: 30px; background: orange');    
-        div.addEventListener('mouseover', function(e) {
-        e.target.style.backgroundColor = 'white';
-        container.appendChild(div)
-    })}else {
-        alert('Please choose a number less than 100');
-        break;
-    }
-}}
-refresh.addEventListener('click', (refreshGrid));
-
+function refreshGrid(){
+    document
+    .querySelectorAll('divs')
+    .forEach((e) => e.parentNode.removeChild(e));
+    rowCol = prompt('How many squares would you like to use?')
+    for (i = 0; i < rowCol * rowCol; i++) {
+        if(rowCol <= 100) {
+         div = document.createElement('div');
+         div.setAttribute('style','width: 30px; height: 30px; background: orange');    
+         div.addEventListener('mouseover', function(e) {
+         e.target.style.backgroundColor = 'white';
+     })}else {
+         alert('Please choose a number less than (or equal to) 100');
+         break;
+     }
+     container.appendChild(div)
+    }}

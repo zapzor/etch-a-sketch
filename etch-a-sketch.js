19989aef11b560142909d6b1b8fd758cfc3a6a3a
Zapzor <imp.4xlbm@aleeas.com>
2021-09-11 23:28:32 +0100

    Refresh button now works correctly. No longer lags

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,48 +1,36 @@
-//Variables and buttons
-const container = document.querySelector('.grid');
-const refresh = document.querySelector('.refresh-button');
-const choose = document.querySelector('.choose');
-refresh.addEventListener('click', (refreshGrid));
-let div;
+// Variables and buttons
+const container = document.querySelector('.grid')
+const refresh = document.querySelector('.refresh-button')
+refresh.addEventListener('click', (refreshGrid))
 makeGridDiv(16)
-//Creates 256 individual divs for 16x16 etch sketch grid
+
+// Creates 256 individual divs for 16x16 etch-a-sketch grid
 function makeGridDiv(rowCol) {
+  container.setAttribute('style',
+  'grid-template-columns: repeat(' + rowCol + ', auto',
+  'grid-template-rows: repeat(' + rowCol + ', auto')
 
-    container.setAttribute('style',
-    'grid-template-columns: repeat(' + rowCol + ', auto',
-    'grid-template-rows: repeat(' + rowCol + ', auto');
+  for (i = 0; i < rowCol * rowCol; i++) {
+    divs = document.createElement('div')
+    divs.setAttribute('style',
+    'background: orange')
+    container.appendChild(divs)
 
-    for (i = 0; i < rowCol * rowCol; i++) {
-        divs = document.createElement('divs');
-        divs.setAttribute('style',
-        'width: 100%; background: orange');
-        divs.addEventListener('mouseover', function(e) {
-            e.target.style.backgroundColor = 'white';
-        });
-        container.appendChild(divs)
+    // Allows grid divs to change color on mouse passover
+    divs.addEventListener('mouseover', function(e) {
+      e.target.style.backgroundColor = 'black'
+    })
 }}
 
-//Button that refreshes grid when clicked
+// Button that refreshes grid when clicked
 function refreshGrid(){
-    document
-    .querySelectorAll('divs')
-    .forEach((e) => e.parentNode.removeChild(e));
 
     rowCol = prompt('How many squares would you like to use?')
-    container.setAttribute('style',
-    'grid-template-columns: repeat(' + rowCol + ', auto',
-    'grid-template-rows: repeat(' + rowCol + ', auto');
-
-    for (i = 0; i < rowCol * rowCol; i++) {
-        if(rowCol <= 100) {
-         div = document.createElement('div');
-         div.setAttribute('style',
-         'width: auto; background: orange');
-     div.addEventListener('mouseover', function(e) {
-         e.target.style.backgroundColor = 'white';
-     })}else {
-         alert('Please choose a number less than or equal to 100');
-         break;
-     }
-     container.appendChild(div)
-    }}
+    if (rowCol <= 100)
+      container.setAttribute('style',
+      'grid-template-columns: repeat(' + rowCol + ', auto',
+      'grid-template-rows: repeat(' + rowCol + ', auto')
+    else {
+      alert('Please choose a number that is less than 100')
+    }
+}

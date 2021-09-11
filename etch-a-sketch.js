a57acdaf556efe50071f8de3c5a6d094f821c225
Zapzor <imp.4xlbm@aleeas.com>
2021-09-11 22:40:52 +0100

    Refactored html and CSS code

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,9 @@
 //Variables and buttons
-const container = document.querySelector('#container');
-const refresh = document.querySelector('#btn');
-const choose = document.querySelector('#chooze');
+const container = document.querySelector('.grid');
+const refresh = document.querySelector('.refresh-button');
+const choose = document.querySelector('.choose');
 refresh.addEventListener('click', (refreshGrid));
-let div; 
+let div;
 makeGridDiv(16)
 //Creates 256 individual divs for 16x16 etch sketch grid
 function makeGridDiv(rowCol) {
@@ -15,7 +15,7 @@ function makeGridDiv(rowCol) {
     for (i = 0; i < rowCol * rowCol; i++) {
         divs = document.createElement('divs');
         divs.setAttribute('style',
-        'width: 100%; background: orange');    
+        'width: 100%; background: orange');
         divs.addEventListener('mouseover', function(e) {
             e.target.style.backgroundColor = 'white';
         });
@@ -27,17 +27,17 @@ function refreshGrid(){
     document
     .querySelectorAll('divs')
     .forEach((e) => e.parentNode.removeChild(e));
-    
+
     rowCol = prompt('How many squares would you like to use?')
     container.setAttribute('style',
     'grid-template-columns: repeat(' + rowCol + ', auto',
     'grid-template-rows: repeat(' + rowCol + ', auto');
-    
+
     for (i = 0; i < rowCol * rowCol; i++) {
         if(rowCol <= 100) {
          div = document.createElement('div');
          div.setAttribute('style',
-         'width: auto; background: orange'); 
+         'width: auto; background: orange');
      div.addEventListener('mouseover', function(e) {
          e.target.style.backgroundColor = 'white';
      })}else {
